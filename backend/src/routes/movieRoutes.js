099eb10375fdadeb4b6deac55cf808be7acdec50
VadaszDaniel2006 <vadaszdana@gmail.com>
2026-03-12 12:57:32 +0100

    Update movieRoutes.js

--- backend/src/routes/movieRoutes.js
+++ backend/src/routes/movieRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
+
+// --- FILMEK VÉGPONTJAI ---
 router.get('/', movieController.getAllMovies);
 router.get('/top50', movieController.getTop50Movies); // ÚJ VÉGPONT
 
